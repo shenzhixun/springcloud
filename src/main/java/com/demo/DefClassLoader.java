caf641d299862ac377b06049228465cdb94ecf27
shenyijie <shenyijie@huixiangtech.cn>
2019-01-24 20:43:57 +0800

    修改配置

---
+++ src/main/java/com/demo/DefClassLoader.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+/**
+ * Copyright (C), 2016-2018, 武汉康华数海有限公司
+ * FileName: DefClassLoader
+ * Author:   ShenYijie
+ * CreateDate:     2018-12-12 15:19
+ * Description:
+ * History:
+ * Version: 1.0
+ */
+public class DefClassLoader {
+
+
+
+}
---
+++ src/main/java/com/demo/EncryptClass.java
@@ -0,0 +1,25 @@
+package com.demo;
+
+/**
+ * Copyright (C), 2016-2018, 武汉康华数海有限公司
+ * FileName: EncryptClass
+ * Author:   ShenYijie
+ * CreateDate:     2018-12-12 15:08
+ * Description: 加密、解密及打包
+ * History:
+ * Version: 1.0
+ */
+public class EncryptClass {
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
